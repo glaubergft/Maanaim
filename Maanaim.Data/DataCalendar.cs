e13f2abb48ff52eb86575f3f44de0af79d7f2936
Glauber Torres <glauber_sp@yahoo.com>
2019-01-24 09:44:08 -0500

    Fix DatabaseName

--- Maanaim.Data/DataCalendar.cs
+++ Maanaim.Data/DataCalendar.cs
@@ -8,24 +8,26 @@ namespace Maanaim.Data
     public class DataCalendar
     {
         private string _connectionString;
+        private string databaseName;
         public DataCalendar(string connectionString)
         {
             _connectionString = connectionString;
+            databaseName = MongoUrl.Create(_connectionString).DatabaseName;
         }
 
         public IEnumerable<Calendar> List()
         {
             IMongoClient client = new MongoClient(_connectionString);
-            IMongoDatabase database = client.GetDatabase("MyFirstDatabase");
-            IMongoCollection<Calendar> colCalendar = database.GetCollection<Calendar>("MyFirstCollectionName");
+            IMongoDatabase database = client.GetDatabase(databaseName);
+            IMongoCollection<Calendar> colCalendar = database.GetCollection<Calendar>("Calendario");
             return colCalendar.AsQueryable<Calendar>();
         }
 
         public void Insert(Calendar calendar)
         {
             IMongoClient client = new MongoClient(_connectionString);
-            IMongoDatabase database = client.GetDatabase("MyFirstDatabase");
-            IMongoCollection<Calendar> colCalendar = database.GetCollection<Calendar>("MyFirstCollectionName");
+            IMongoDatabase database = client.GetDatabase(databaseName);
+            IMongoCollection<Calendar> colCalendar = database.GetCollection<Calendar>("Calendario");
             colCalendar.InsertOne(calendar);
         }
     }
